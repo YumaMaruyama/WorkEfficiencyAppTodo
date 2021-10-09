05db42d81a0445292b56d22d7350bd35789f73fb
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-10-09 22:21:28 +0900

    jarファイル　デーモン化

--- src/main/java/com/example/demo/SpringBoot_TodoApplication.java
+++ src/main/java/com/example/demo/SpringBoot_TodoApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class SpringBoot_TodoApplication {
+public class SpringBoot_TodoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot_TodoApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(SpringBoot_TodoApplication.class);
+	  }
 }
